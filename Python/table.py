773dc905eac79ff998d2ccbf674accab6b36dca9
vanshtry1101 <try.web110@gmail.com>
2025-06-28 10:11:38 +0530

    Add files via upload

---
+++ Python/table.py
@@ -0,0 +1,9 @@
+k=int(input("Enter Table:="))
+
+# Method One
+# for i in range(1,11):
+        # print(f"{k} x {i} = {i*k} ")
+
+# Method Two
+for i in range(1,11):
+    print(k,"*",i,"=",i*k)        
