8648110415b230bbc4ad338c79f280d87e71db1a
vanshtry1101 <try.web110@gmail.com>
2025-07-26 18:33:56 +0530

    Delete table.py

--- table.py
+++
@@ -1,9 +0,0 @@
-k=int(input("Enter Table:="))
-
-# Method One
-# for i in range(1,11):
-        # print(f"{k} x {i} = {i*k} ")
-
-# Method Two
-for i in range(1,11):
-    print(k,"*",i,"=",i*k)        
